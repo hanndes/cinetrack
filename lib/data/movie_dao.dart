f94f641152e3a53d3d8ca34df8563576bb564a33
hanndes <handederelii@hotmail.com>
2026-08-10 13:34:53 +0300

    feat: movies tablosuna 20 film eklendi, MovieDao oluşturuldu

---
+++ lib/data/movie_dao.dart
@@ -0,0 +1,40 @@
+import '../models/movie.dart';
+import 'DatabaseHelper.dart';
+
+class MovieDao {
+  final dbHelper = DatabaseHelper.instance;
+
+  // Tek film ekle
+  Future<int> insertMovie(Movie movie) async {
+    final db = await dbHelper.database;
+    return await db.insert('movies', movie.toMap()..remove('id'));
+  }
+
+  // Tüm filmleri getir
+  Future<List<Movie>> getAllMovies() async {
+    final db = await dbHelper.database;
+    final maps = await db.query('movies');
+    return maps.map((map) => Movie.fromMap(map)).toList();
+  }
+
+  // ID ile tek film getir
+  Future<Movie?> getMovieById(int id) async {
+    final db = await dbHelper.database;
+    final maps = await db.query(
+      'movies',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (maps.isNotEmpty) {
+      return Movie.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  // Tablo boş mu kontrol et (seed işlemi için)
+  Future<bool> isEmpty() async {
+    final db = await dbHelper.database;
+    final result = await db.query('movies', limit: 1);
+    return result.isEmpty;
+  }
+}
--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,4 +1,5 @@
 class Movie {
+  final int? id;
   final String title;
   final String director;
   final double imdbRating;
@@ -10,6 +11,7 @@ class Movie {
   final List<String> genres;
 
   Movie({
+    this.id,
     required this.title,
     required this.director,
     required this.imdbRating,
@@ -20,4 +22,33 @@ class Movie {
     required this.cast,
     required this.genres,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'posterUrl': posterUrl,
+      'director': director,
+      'imdbRating': imdbRating,
+      'badge': badge,
+      'durationMinutes': durationMinutes,
+      'releaseYear': releaseYear,
+      'genres': genres.join(','),
+    };
+  }
+
+  factory Movie.fromMap(Map<String, dynamic> map) {
+    return Movie(
+      id: map['id'],
+      title: map['title'],
+      posterUrl: map['posterUrl'],
+      director: map['director'],
+      imdbRating: map['imdbRating'],
+      badge: map['badge'],
+      durationMinutes: map['durationMinutes'],
+      releaseYear: map['releaseYear'],
+      genres: (map['genres'] as String).split(','),
+      cast: const [],
+    );
+  }
 }
